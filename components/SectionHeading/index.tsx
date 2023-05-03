e03e155459e3a1bba9fcc07b3a71560cbb85c8f6
Adithyan A <deepaadithyan56@gmail.com>
2023-05-03 22:35:23 +0530

    AboutMe page created

---
+++ components/SectionHeading/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {
+  title: string;
+};
+
+export default function SectionHeading({ title }: Props) {
+  return (
+    <motion.h1
+      initial={{y: "100%", opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{duration: 1}}
+      className="pt-4 pb-5 lg:pt-5 lg:pb-6 uppercase tracking-widest font-medium first-letter:text-2xl text-lg lg:first-letter:text-3xl lg:text-2xl text-gray-400 first-letter:text-yellow-500"
+    >
+      {title}
+    </motion.h1>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import HomeSection from "../components/HomeSection";
+import AboutSection from "../components/AboutSection";
 
 export default function Home() {
   return (
-    <div>
-    <div className="bg-gray-1000 h-screen snap-y snap-mandatory scroll-smooth overflow-y-auto overflow-x-hidden z-0">
+    <div className="bg-gray-1000 h-screen snap-y snap-mandatory scroll-smooth overflow-x-hidden overflow-y-auto">
       <Head>
         <title>Adithyan's Portfolio</title>
       </Head>
 
       <Navbar />
       <HomeSection />
+      <AboutSection />
 
-      {/* About Section */}
-      {/* <AboutSection /> */}
 
       {/* Skills Section */}
       {/* <SkillsSection /> */}
