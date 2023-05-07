44637d1206ec7f7fcd2dca6e70823ed78a09f4d2
Adithyan A <deepaadithyan56@gmail.com>
2023-05-07 18:00:21 +0530

    Experience removed from navlinks

--- utils/navigation.ts
+++ utils/navigation.ts
@@ -3,11 +3,9 @@ type NavLink = {
   url: string;
 };
 
-
 export const navLinks: NavLink[] = [
   { title: "Home", url: "#home" },
   { title: "About", url: "#about" },
   { title: "Skills", url: "#skills" },
   { title: "Projects", url: "#projects" },
-  { title: "Experience", url: "#experience" },
 ];
