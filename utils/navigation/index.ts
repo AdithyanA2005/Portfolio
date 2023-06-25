46ce2f3c3431c9edc8a604028106b1f5941dbbf5
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-06-25 07:43:37 +0000

    Navigation navlinks optimised

--- utils/navigation/index.ts
+++ utils/navigation/index.ts
@@ -1,10 +1,10 @@
 import { NavLink } from "./typings";
 
 export const navLinks: NavLink[] = [
-  { title: "Home", url: "#home" },
-  { title: "About", url: "#about" },
-  { title: "Skills", url: "#skills" },
-  { title: "Projects", url: "#projects" },
-  { title: "Blogs", url: "#blogs" },
-  { title: "Contact", url: "#contact" },
+  { title: "Home", url: "/#home" },
+  { title: "About", url: "/#about" },
+  { title: "Skills", url: "/#skills" },
+  { title: "Projects", url: "/#projects" },
+  { title: "Blogs", url: "/#blogs" },
+  { title: "Contact", url: "/#contact" },
 ];
