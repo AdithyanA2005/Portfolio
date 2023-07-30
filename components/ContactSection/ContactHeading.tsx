8604c1ae556aad505716cb1c738795d427ec51ee
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-07-30 14:56:59 +0000

    More SEO optimisation

--- components/ContactSection/ContactHeading.tsx
+++ components/ContactSection/ContactHeading.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 export default function ContactHeading({ }: Props) {
   return (
-    <h4 className="text-center text-3xl font-semibold text-white">
+    <h2 className="text-center text-3xl font-semibold text-white">
       I have got what you just need.{" "}
       <span className="decoration-yellow-500 underline">Lets Talk.</span>
-    </h4>
+    </h2>
   );
 }
